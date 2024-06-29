e73f9feb991277405df5dfe0a026b8a825239ab8
weibaoan <3255427824@qq.com>
2024-06-29 22:50:46 +0800

    banner

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,5 +1,13 @@
 <template>
 	<view class="homeLayout">
+		<view class="banner">
+			<swiper indicator-dots indicator-color=rgba(255,255,255,0.5) indicator-active-color="#fff" autoplay circular>
+				<swiper-item v-for="item in 3" >
+					<image src="../../common/images/banner1.jpg" mode="aspectFill"></image>
+				</swiper-item>
+			
+			</swiper>
+		</view>
 	
 		
 	</view>
@@ -10,6 +18,29 @@
 </script>
 
 <style lang="scss" scoped>
-	
+	.homeLayout {
+		.banner {
+			width: 750rpx;
+			padding: 30rpx 0;
+			
+			swiper {
+				width: 750rpx;
+				height: 340rpx;
+				
+				&-item {
+					width: 100%;
+					height: 100%;
+					padding: 0 30rpx;
+					
+					image {
+						width: 100%;
+						height: 100%;
+						border-radius: 10rpx;
+					}
+				}
+			}
+			
+		}
+	}
 	
 </style>
