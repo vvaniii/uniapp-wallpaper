8926bb40cb6210d93ddc6868c29d0626f2721f8b
weibaoan <3255427824@qq.com>
2024-06-30 15:12:35 +0800

    tabBar

---
+++ unpackage/dist/dev/mp-weixin/pages/user/user.js
@@ -0,0 +1,8 @@
+"use strict";
+const common_vendor = require("../../common/vendor.js");
+const _sfc_main = {};
+function _sfc_render(_ctx, _cache) {
+  return {};
+}
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "C:/Users/wei/Desktop/WEB/uniappVue3/wallpaper/pages/user/user.vue"]]);
+wx.createPage(MiniProgramPage);
