6ebf160c7566bb68ab50a07c169f565c0c01bda7
weibaoan <3255427824@qq.com>
2024-06-30 15:57:15 +0800

    user style

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -1,6 +1,45 @@
 <template>
-  <view class="">
+  <view class="userLayout">
+	  <view class="userInfo">
+		  <view class="avatar">
+		  	<image src="../../static/images/xxmLogo.png" mode="aspectFill"></image>
+		  </view>
+		  <view class="nickname">100.100.100.100</view>
+		  <view class="address">来自于：未来</view>
+	  </view>
+	  <view class="section">
+		  <view class="list">
+			  <view class="row" v-for="(item,index) in 3">
+			  	<view class="left">
+					<uni-icons type="download-filled" size="20"  color="#28b389"></uni-icons>
+					<view class="text">
+						我的下载
+					</view>
+				</view>
+				<view class="right">
+					<view class="text">23</view>
+					<uni-icons type="right" size="15" color="#aaa"></uni-icons>
+				</view>
+			  </view>
+		  </view>
+		  </view>
+	  <view class="section"> 
+	  <view class="list">
+	  			  <view class="row" v-for="(item,index) in 2">
+	  			  	<view class="left">
+	  					<uni-icons type="download-filled" size="20" color="#28b389"></uni-icons>
+	  					<view class="text">
+	  						我的下载
+	  					</view>
+	  				</view>
+	  				<view class="right">
+	  					<view class="text">23</view>
+	  					<uni-icons type="right" size="15" color="#aaa"></uni-icons>
+	  				</view>
+	  			  </view>
+	  </view>
 	  
+	  </view>
 	  </view>
 </template>
 
@@ -8,6 +47,78 @@
 	
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" scoped>	
+.userLayout {
+	.userInfo {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			flex-direction: column;
+			padding: 50rpx 0;
+			
+			.avatar {
+				width: 160rpx;
+				height: 160rpx;
+				border-radius: 50%;
+				overflow: hidden;
+				image {
+					width: 100%;
+					height: 100%;
+				}
+				
+			}
+			.nickname {
+				font-size: 44rpx;
+				color: #333;
+				padding: 20rpx 0 5rpx;
+				
+			}
+			.address {
+				font-size: 28rpx;
+				color: #ababab;
+			}
+		}
+		
+		.section {
+			width: 690rpx;
+			margin: 50rpx auto;
+			border: 1rpx  solid #eee;
+			border-radius: 10rpx;
+			box-shadow: 0 0 30rpx rgba(0,0,0,0.05);
+			.list {
+				.row {
+					display: flex;
+					justify-content: space-between;
+					 align-items: center;
+					padding: 0 30rpx;
+					height: 100rpx;
+					border-bottom: 1rpx solid #eee;
+					
+					 &:last-child {
+						 border-bottom: 0;
+					 }
+					 .left {
+						 display: flex;
+						 align-items: center;
+						 .text {
+							 padding-left: 20rpx;
+							 color: #666;
+						 }
+					 }
+					 .right {
+					 	display: flex;
+					 	align-items: center;
+						.text {
+							font-size: 28rpx;
+							color: #aaa;
+						}
+					 }
+				}
+			
+				
+			}
+		} 
+	
+	}
 	
 </style>
--- unpackage/dist/dev/mp-weixin/pages/user/user.js
+++ unpackage/dist/dev/mp-weixin/pages/user/user.js
@@ -1,8 +1,49 @@
 "use strict";
 const common_vendor = require("../../common/vendor.js");
 const _sfc_main = {};
-function _sfc_render(_ctx, _cache) {
-  return {};
+if (!Array) {
+  const _easycom_uni_icons2 = common_vendor.resolveComponent("uni-icons");
+  _easycom_uni_icons2();
 }
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "C:/Users/wei/Desktop/WEB/uniappVue3/wallpaper/pages/user/user.vue"]]);
+const _easycom_uni_icons = () => "../../uni_modules/uni-icons/components/uni-icons/uni-icons.js";
+if (!Math) {
+  _easycom_uni_icons();
+}
+function _sfc_render(_ctx, _cache) {
+  return {
+    a: common_vendor.f(3, (item, index, i0) => {
+      return {
+        a: "0f7520f0-0-" + i0,
+        b: "0f7520f0-1-" + i0
+      };
+    }),
+    b: common_vendor.p({
+      type: "download-filled",
+      size: "20",
+      color: "#28b389"
+    }),
+    c: common_vendor.p({
+      type: "right",
+      size: "15",
+      color: "#aaa"
+    }),
+    d: common_vendor.f(2, (item, index, i0) => {
+      return {
+        a: "0f7520f0-2-" + i0,
+        b: "0f7520f0-3-" + i0
+      };
+    }),
+    e: common_vendor.p({
+      type: "download-filled",
+      size: "20",
+      color: "#28b389"
+    }),
+    f: common_vendor.p({
+      type: "right",
+      size: "15",
+      color: "#aaa"
+    })
+  };
+}
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-0f7520f0"], ["__file", "C:/Users/wei/Desktop/WEB/uniappVue3/wallpaper/pages/user/user.vue"]]);
 wx.createPage(MiniProgramPage);
